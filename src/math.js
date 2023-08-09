50965c418b49da3c4f6b3584d8896d1880de609f
MarcoDayz <diaz.marco8686@gmail.com>
2023-08-09 14:00:17 -0700

    updated math.js; added parameters to genProblem function

--- src/math.js
+++ src/math.js
@@ -1,5 +1,41 @@
-export const genProblem = () => {
-    const equation = {
+//genProblem function generates a problem
+export const genProblem = (subject) => {
+    let equation = {
+        solution: 0,
+        problem: ""
+    };
+    if(subject === "" || subject === undefined){
+        return equation;
+    }
+    const num1 = genRandomNumber();
+    const num2 = genRandomNumber();
+
+    if(subject === "+"){
+        equation.solution = num1 + num2;
+        equation.problem = `${num1} + ${num2}`;
+    }else if(subject === "-"){
+        if(num2 > num1){
+            equation.solution = num2 - num1;
+            equation.problem = `${num2} - ${num1}`;
+            return equation;
+        }
+        equation.solution = num1 - num2;
+        equation.problem = `${num1} - ${num2}`;
+    }else if(subject === "*"){
+        equation.solution = num1 * num2;
+        equation.problem = `${num1} x ${num2}`;
+    }else{
+        equation = genRandomProblem();
+    }
+    return equation;
+};
+
+const genRandomNumber = () => {
+    return Math.floor(Math.random() * 13);
+};
+
+const genRandomProblem = () => {
+        const equation = {
         solution: 0,
         problem: ""
     };
@@ -7,7 +43,7 @@ export const genProblem = () => {
     const num2 = genRandomNumber();
     const equationArr = ["+","-","*"]
     const randomEquation = equationArr[Math.floor(Math.random() * 3)];
-    if(randomEquation === "+"){
+        if(randomEquation === "+"){
         equation.solution = num1 + num2;
         equation.problem = `${num1} + ${num2}`;
     }else if(randomEquation === "-"){
@@ -22,9 +58,12 @@ export const genProblem = () => {
         equation.solution = num1 * num2;
         equation.problem = `${num1} x ${num2}`;
     }
-    return equation;
-};
 
-const genRandomNumber = () => {
-    return Math.floor(Math.random() * 13);
-};
+    return equation;
+}
+
+// console.log(genProblem())
+// console.log(genProblem("+"))
+// console.log(genProblem("-"))
+// console.log(genProblem("*"))
+// console.log(genProblem("combo"))
